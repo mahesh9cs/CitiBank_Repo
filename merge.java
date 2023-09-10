2395c48abd31a6a3abf7ffae87fb2fd27f35c6c6
mahesh9Cs <maheshmumbai@gmail.com>
2023-09-10 10:20:19 +0530

    mew loops concepts have been addedd

--- merge.java
+++ merge.java
@@ -1,3 +1,12 @@
+public class ForExample {  
+public static void main(String[] args) {  
+    //Code of Java for loop  
+    for(int i=1;i<=10;i++){  
+        System.out.println(i);  
+    }  
+}  
+}
+
 public class MultiplicationTable {
 
     public static void main(String[] args) {
